72948a2c727f98001e75986cb64b1f2cc16540f7
Aloïs Deniel <alois.deniel@gmail.com>
2020-12-04 06:57:41 +0100

    Added `expirationDate` and `expirationDuration`.

--- lib/src/future.dart
+++ lib/src/future.dart
@@ -2,15 +2,17 @@ import 'package:meta/meta.dart';
 
 import 'update.dart';
 
-/// Describes the behaviour of an update when one is already running.
-enum UpdateOverride {
+/// Describes the behaviour of an update when one is already running or expired.
+abstract class UpdateOverride {
+  const UpdateOverride._();
+
   /// If a new update is requested and there already an ongoing update, the new update is ignored
   /// and the previous one continues.
-  ignore,
+  static const UpdateOverride ignore = _UpdateOverrideIgnore();
 
   /// If a new update is requested and there already an ongoing update, the previous one is cancelled
   /// and the new update starts.
-  cancelPrevious,
+  static const UpdateOverride cancelPrevious = _UpdateOverrideCancelPrevious();
 }
 
 /// Starts a sequence of updates by running the [updater] and raising a new [Update] after each step of its execution.
@@ -22,11 +24,19 @@ enum UpdateOverride {
 /// by the [override] parameters. The new update can whether be ignore, or cancel the previous execution.
 ///
 /// An [optimisticValue] can be given to display an anticipated result during the loading phase.
+///
+/// If [expirationDate] is given, and the update has already been updated, then a new update is triggered only if the
+/// the given date is before [DateTime.now].
+///
+/// If [expirationDuration] is given, and no [expirationDate] is given, then [expirationDate] becomes the last updated
+/// date with the duration added to it.
 Stream<Update<T>> update<T>({
   @required Future<T> Function() updater,
   @required Update<T> Function() getUpdate,
   UpdateOverride override = UpdateOverride.ignore,
   T optimisticValue,
+  DateTime expirationDate,
+  Duration expirationDuration,
 }) {
   assert(getUpdate != null);
   assert(updater != null);
@@ -54,7 +64,7 @@ Stream<Update<T>> update<T>({
       }
     },
     updating: (state) async* {
-      if (override == UpdateOverride.cancelPrevious) {
+      if (override is _UpdateOverrideCancelPrevious) {
         final updating = Updating.cancelling(
           state,
           id: _createId(),
@@ -78,24 +88,31 @@ Stream<Update<T>> update<T>({
       }
     },
     updated: (state) async* {
-      final updating = Refreshing.fromUpdated(
-        state,
-        id: _createId(),
-        optimisticValue: optimisticValue,
-      );
-      yield updating;
-      try {
-        final result = await updater();
-        if (!getUpdate().isCancelled(updating.id)) {
-          yield Updated.fromRefreshing(updating, result);
-        }
-      } catch (error, stackTrace) {
-        if (!getUpdate().isCancelled(updating.id)) {
-          yield FailedRefresh.fromRefreshing(
-            updating,
-            error: error,
-            stackTrace: stackTrace,
-          );
+      if (expirationDuration != null) {
+        expirationDate ??= state.updatedAt.add(expirationDuration);
+      }
+      final isExpired =
+          expirationDate != null && expirationDate.isBefore(DateTime.now());
+      if (isExpired) {
+        final updating = Refreshing.fromUpdated(
+          state,
+          id: _createId(),
+          optimisticValue: optimisticValue,
+        );
+        yield updating;
+        try {
+          final result = await updater();
+          if (!getUpdate().isCancelled(updating.id)) {
+            yield Updated.fromRefreshing(updating, result);
+          }
+        } catch (error, stackTrace) {
+          if (!getUpdate().isCancelled(updating.id)) {
+            yield FailedRefresh.fromRefreshing(
+              updating,
+              error: error,
+              stackTrace: stackTrace,
+            );
+          }
         }
       }
     },
@@ -144,7 +161,7 @@ Stream<Update<T>> update<T>({
       }
     },
     refreshing: (state) async* {
-      if (override == UpdateOverride.cancelPrevious) {
+      if (override is _UpdateOverrideCancelPrevious) {
         final updating = Refreshing.cancelling(
           state,
           id: _createId(),
@@ -190,3 +207,11 @@ int _createId() => _lastId++;
 /// The unique identifiers associated to updates.
 int _lastId = DateTime.now().millisecondsSinceEpoch -
     DateTime(2020, 1, 1).millisecondsSinceEpoch;
+
+class _UpdateOverrideIgnore extends UpdateOverride {
+  const _UpdateOverrideIgnore() : super._();
+}
+
+class _UpdateOverrideCancelPrevious extends UpdateOverride {
+  const _UpdateOverrideCancelPrevious() : super._();
+}
